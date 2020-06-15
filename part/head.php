2ce27f260aa8bbeeccfb5d3042317d3e5be5d654
unknown <her2237>
2020-06-15 17:45:38 +0900

    아이콘사이드바 작업

--- part/head.php
+++ part/head.php
@@ -1,48 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>블로그</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
-    <link rel="stylesheet" href="/resource/common.css">
-    <script src="/resource/common.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>블로그</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
+  <link rel="stylesheet" href="/resource/common.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="/resource/common.js"></script>
 </head>
+
 <body>
-    <div class="top-bar">
-        <div class="con height-100p flex">
-            <a href="/" class="logo flex flex-ai-c">
-                <i class="fas fa-clone"></i>
-            </a>
-            <nav class="menu-box-1 flex flex-1-0-0">
-                <ul class="flex flex-1-0-0">
-                    <li class="flex-1-0-0 flex"><a href="/" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Home</a></li>
-                    <li class="flex-1-0-0 flex"><a href="/list.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Articles</a>
-                    </li>
-                    <li class="flex-1-0-0 flex"><a href="/aboutMe.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">About Me</a>
-                    </li>
-                    <li class="flex-1-0-0 flex"><a href="#" class="flex flex-1-0-0 flex-ai-c flex-jc-c">SNS</a></li>
-                    <li class="flex-1-0-0 flex">
-                        <a href="#" class="flex flex-1-0-0 flex-ai-c flex-jc-c">SNS</a>
-                        <ul>
-                            <li>
-                                <a href="https://github.com/jhs512" target="_blank">
-                                    GITHUB
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://github.com/jhs512" target="_blank">
-                                    FACEBOOK
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://github.com/jhs512" target="_blank">
-                                    INSTA
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+  <div class="mobile-top-bar visible-sm-down flex">
+    <a href="#" class="btn-toggle-mobile-side-bar flex-as-c">
+      <div></div>
+      <div></div>
+      <div></div>
+    </a>
+  </div>
+  <div class="mobile-side-bar">
+      <ul>
+          <li><a href="#" class="block">메뉴</a></li>
+          <li><a href="#" class="block">메뉴</a></li>
+          <li><a href="#" class="block">메뉴</a></li>
+      </ul>
+  </div>
+  <div class="top-bar visible-md-up">
+    <div class="con height-100p flex">
+      <a href="/" class="logo flex flex-ai-c">
+        <i class="fas fa-clone"></i>
+      </a>
+      <nav class="menu-box-1 flex flex-1-0-0">
+        <ul class="flex flex-1-0-0">
+          <li class="flex-1-0-0 flex"><a href="/" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Home</a></li>
+          <li class="flex-1-0-0 flex"><a href="/list.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">Articles</a>
+          </li>
+          <li class="flex-1-0-0 flex"><a href="/aboutMe.php" class="flex flex-1-0-0 flex-ai-c flex-jc-c">About Me</a>
+          </li>
+          <li class="flex-1-0-0 flex">
+            <a href="#" class="flex flex-1-0-0 flex-ai-c flex-jc-c">SNS</a>
+            <ul>
+              <li>
+                <a href="https://github.com/jhs512" target="_blank">
+                  GITHUB
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/jhs512" target="_blank">
+                  FACEBOOK
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/jhs512" target="_blank">
+                  INSTA
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
     </div>
+  </div>
+</body>
---
+++ public/resource/common.js
@@ -0,0 +1,20 @@
+console.clear();
+function MobileSideBar__toggle() {
+    var $btn = $(".btn-toggle-mobile-side-bar");
+  
+    if ($btn.hasClass("active")) {
+      $btn.removeClass("active");
+      $(".mobile-side-bar").removeClass("active");
+    } else {
+      $btn.addClass("active");
+      $(".mobile-side-bar").addClass("active");
+    }
+  }
+  
+  function MobileSideBar__init() {
+    $(".btn-toggle-mobile-side-bar").click(MobileSideBar__toggle);
+  }
+  $(function () {
+    MobileSideBar__init();
+  });
+  
