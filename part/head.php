916a08bed9960266c20c138775b41df6886be52f
her2237 <her2237@gmail.com>
2020-06-19 18:15:11 +0900

    슬라이드바 수정

--- part/head.php
+++ part/head.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>블로그</title>
@@ -9,8 +10,16 @@
   <link rel="stylesheet" href="/resource/common.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
     integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+  <link rel="icon" href="../resource/img/favicon.ico">
+  
+  
+  
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="/resource/common.js"></script>
+
+  
+
 </head>
 
 <body>
