6580c25bd8ee05fe793cbbdc7a0db65fec2ab5e4
her2237 <her2237@gmail.com>
2020-07-14 18:47:19 +0900

    밀알복지재단 추가

--- public/pf/밀알복지재단/index.php
+++ public/pf/밀알복지재단/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +11,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="/pf/밀알복지재단/index.js"></script>
 </head>
+
 <body>
     <div class="top-bar flex flex-jc-c flex-ai-c">
         <ul class="flex">
@@ -20,7 +22,10 @@
             <li><a href="#">KOR</a></li>
         </ul>
     </div>
-    <div class= "twice-bar flex flex-jc-c flex-ai-c">
+    <div class="twice-bar flex flex-jc-c flex-ai-c">
+        <div class="logo flex">
+            <img src="https://her2237.github.io/img1/pf/mile/logo.png" alt="">
+        </div class="menu-bar">
         <ul class="flex">
             <li><a href="#">재단소개</a></li>
             <li><a href="#">사업소개</a></li>
@@ -29,5 +34,61 @@
             <li><a href="#">나의후원공간</a></li>
         </ul>
     </div>
+
+
+    <div class="main-slider">
+        <div class="slides">
+            <div
+                style="background-image:url(https://her2237.github.io/img1/pf/mile/20200625RDCVREIS2YFSAD3DPM18XYSO.jpg);">
+            </div>
+        </div>
+    </div>
+    <div class="page-1">
+        <div>
+            <div class=" title ">후원 캠페인</div>
+            <div class="little-title "> 소외된 이웃들을 위해 특별하고 아름다운 사랑을 실천합니다.</div>
+        </div>
+        <div class="list-img flex flex-jc-c flex-ai-c">
+            <div class="flex">
+                <div class="img-box"> 
+                    <a href="#">
+                        <img src="https://her2237.github.io/img1/pf/mile/20200414BQ4ZCHUA453IIU9UO2BNQAFU.jpg" alt=""
+                            srcset="">
+                    </a>
+                </div>
+                <div class="img-box">
+                    <a href="#">
+                        <img src="https://her2237.github.io/img1/pf/mile/20200515CCYG8ALNBXRMFPX7S3WFPHHD.jpg" alt=""
+                            srcset="">
+                    </a>
+                </div>
+                <div class="img-box">
+                    <a href="#">
+                        <img src="https://her2237.github.io/img1/pf/mile/20200519ZPI3A35M8AX50LPI9TGG7N91.jpg" alt=""
+                            srcset="">
+                    </a>
+                </div>
+                <div class="img-box">
+                    <div class=img-hand
+                        style="background-image:url(https://her2237.github.io/img1/pf/mile/sponsor_campain_4.jpg);">
+
+                        <div class="text-1">후원이 처음이신가요?</div>
+                        <div class="text-2">누구나 쉽게 참여할 수 있는
+                            <br>
+                            사랑의 실천 밀알복지재단 후원을
+                            <br>
+                            안내해드립니다.</div>
+                        <div class="btn-middle">
+                            <div class="btn-cricle">
+                                <span>후원안내</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
+
 </html>
