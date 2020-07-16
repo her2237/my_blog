5dab5e6a8f36c85b761eb1f2f7e85950815166c2
her2237 <her2237@email.com>
2020-07-16 18:51:48 +0900

    추추가

--- public/pf/밀알복지재단/index.php
+++ public/pf/밀알복지재단/index.php
@@ -6,6 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>밀알복지재단</title>
+    <!-- 구글 폰트 불러오기 -->
+    <!-- rotobo(400/700/900), notosanskr(400/600/900) -->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&family=Roboto:wght@400;700;900&display=swap"
+        rel="stylesheet">
+
+    <!-- 폰트어썸 불러오기 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
@@ -212,10 +220,10 @@
             <div class="main-img-af">
                 <img src="https://her2237.github.io/img1/pf/mile/20200714LONNLSAH6QNIHQKLUND789LP.jpg" alt="">
             </div>
-            <div class="green-aide">
-
-            </div>
         </div>
+
+
+
         <div class="page-6">
             <div class="news-title">뉴스레터 신청하기</div>
             <div class="apply flex flex-jc-c flex-ai-c">
@@ -235,16 +243,98 @@
                         <li> 밀알과 함께하는 기업</li>
                         <li><img src="https://her2237.github.io/img1/pf/mile/20180422DEXDY1EFQ2282TRJ1NQPO6WL.jpg"
                                 alt=""></li>
-                        <li><img src="https://her2237.github.io/img1/pf/mile/20180422FDBM4BZJZFOKFBL5BT94NGRN (1).jpg" alt=""></li>
-                        <li><img src="https://her2237.github.io/img1/pf/mile/20180422HFKZEJMQI6TLTXI4KBQQ8A9I (1).jpg" alt=""></li>
-                        <li><img src="https://her2237.github.io/img1/pf/mile/20180422G7LYBYOS0YQMLD2IV5QBJRG0 (1).jpg" alt=""></li>
-                        <li><img src="https://her2237.github.io/img1/pf/mile/20180422J79L588XFQ2LYCM0CJIFNS1O.jpg" alt=""></li>
-                        <li><img src="https://her2237.github.io/img1/pf/mile/20180422V37H3L40FAT1W2MA647AQ4RY.jpg" alt=""></li>
+                        <li><img src="https://her2237.github.io/img1/pf/mile/20180422FDBM4BZJZFOKFBL5BT94NGRN.jpg"
+                                alt=""></li>
+                        <li><img src="https://her2237.github.io/img1/pf/mile/20180422HFKZEJMQI6TLTXI4KBQQ8A9I.jpg"
+                                alt=""></li>
+                        <li><img src="https://her2237.github.io/img1/pf/mile/20180422G7LYBYOS0YQMLD2IV5QBJRG0.jpg"
+                                alt=""></li>
+                        <li><img src="https://her2237.github.io/img1/pf/mile/20180422J79L588XFQ2LYCM0CJIFNS1O.jpg"
+                                alt=""></li>
+                        <li><img src="https://her2237.github.io/img1/pf/mile/20180422V37H3L40FAT1W2MA647AQ4RY.jpg"
+                                alt=""></li>
                     </ul>
                 </div>
             </div>
         </div>
-    </div>
+        <div class="page-8">
+            <div class="black-bottom-1">
+                <div class="intro">
+                    <ul class="flex">
+                        <li>밀알복지재단 소개</li>
+                        <li>이용약관</li>
+                        <li>개인정보처리방침</li>
+                        <li>이메일무단수집거부</li>
+                        <li>오시는 길</li>
+                    </ul>
+                </div>
+                <div>
+                    1111
+                    <div class="icon-1"></div>
+                    <div class="icon-2"></div>
+                </div>
+            </div>
+        </div>
+        <div class="page-end">
+            <div class="last-list">
+                <ul class="flex">
+                    <li>사회복지밀알복지 재단</li>
+                    <li>대표자 홍정길</li>
+                    <li> 사업자등록번호213-82-04651</li>
+                </ul>
+                <ul>
+                    <li>(우)06349 서울특별시 강남구 밤고개로 1길 34 (수서동)
+                    </li>
+                </ul>
+                <ul class="flex">
+                    <li>
+                        대표전화 02-3411-4664
+                    </li>
+                    <li>
+                        후원자전용번호 1600-0966 (평일 AM 9:00 ~ PM 6:00)
+                    </li>
+                </ul>
+                <ul>
+                    <li>COPYRIGHTⒸ 2018 MIRAL ALL RIGHTS RESERVED.</li>
+                </ul>
+                <div class="money">
+                    <ul>
+                        <li>후원전용계좌</li>
+                        <li>KEB하나은행810-213140-01605(예금주: 사회복지법인 밀알복지재단)</li>
+                        <li>기부금 영수증 등 후원내역 관리를 위해서 계좌입금 후 연락부탁드립니다.</li>
+                    </ul>
+                </div>
+            </div>
+
+
+            <div class="box-1 active-on-visible con flex" data-active-on-visible-callback-func-name="count">
+                <div class="flex-1-0-0 flex border-red">
+                    <div class="border-green relative flex flex-jc-e">
+                        <div class="border-gold relative flex flex-ai-e flex-jc-c">
+                            <div>
+                                <span class="num" data-num-ani-slow-point="0.9" data-num-ani-slow-add-interval="10"
+                                    data-num-ani-interval="40" data-num-ani-stride="41" data-num-ani-start="0"
+                                    data-num-ani-end="8785"></span>명
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="icon-circle"></div>
+                </div>
+                <div class="flex-1-0-0 flex flex-jc-e border-red">
+                    <div class="border-green relative flex">
+                        <div class="border-gold relative flex flex-ai-e flex-jc-c">
+                            <div>
+                                <span class="num" data-num-ani-slow-point="0.9" data-num-ani-slow-add-interval="10"
+                                    data-num-ani-interval="40" data-num-ani-stride="41" data-num-ani-start="0"
+                                    data-num-ani-end="8785"></span>명
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 </body>
 
 </html>
--- public/resource/common.js
+++ public/resource/common.js
@@ -74,3 +74,114 @@ function a() {
 }
 
 setInterval(a, 5000);
+
+/* 숫자 카운트 라이브러리 시작 */
+function NumAni__start(selector) {
+  $(selector).each(function(index, node) {
+      var $el = $(node);
+      
+      var start = parseInt($el.attr('data-num-ani-start'));
+      var interval = parseInt($el.attr('data-num-ani-interval'));
+
+      $el.attr('data-num-ani-interval-current', interval);
+
+      $el.text(start);
+
+      NumAni__increaseNum($el);
+  });
+}
+
+function NumAni__increaseNum($el) {
+  var current = parseInt($el.text());
+  var end = parseInt($el.attr('data-num-ani-end'));
+  var stride = parseInt($el.attr('data-num-ani-stride'));
+  var interval = parseInt($el.attr('data-num-ani-interval-current'));
+  var slowPoint = parseFloat($el.attr('data-num-ani-slow-point'))
+
+  if (current < end) {
+      if ( current > end * slowPoint ) {
+          interval += parseInt($el.attr('data-num-ani-slow-add-interval'));
+          if ( interval > 100 ) {
+              interval = 100;
+          }
+          $el.attr('data-num-ani-interval-current', interval);
+      }
+
+      if ( current + stride > end ) {
+          $el.text(end);
+      }
+      else {
+          $el.text(current + stride);
+      }
+
+      setTimeout(function () {
+          NumAni__increaseNum($el);
+      }, interval);
+  }
+  else {
+      $el.addClass('num-action-done');
+  }
+}
+/* 숫자 카운트 라이브러리 끝 */
+
+/* 발견되면 활성화시키는 라이브러리 시작 */
+function ActiveOnVisible__init() {
+  $(window).resize(ActiveOnVisible__initOffset);
+  ActiveOnVisible__initOffset();
+
+  $(window).scroll(ActiveOnVisible__checkAndActive);
+  ActiveOnVisible__checkAndActive();
+}
+
+function ActiveOnVisible__initOffset() {
+  $('.active-on-visible').each(function(index, node) {
+      var $node = $(node);
+
+      var offsetTop = $node.offset().top;
+      $node.attr('data-active-on-visible-offsetTop', offsetTop);
+
+      if ( !$node.attr('data-active-on-visible-diff-y') ) {
+          $node.attr('data-active-on-visible-diff-y', '0');
+      }
+
+      if ( !$node.attr('data-active-on-visible-delay') ) {
+          $node.attr('data-active-on-visible-delay', '0');
+      }
+  });
+
+  ActiveOnVisible__checkAndActive();
+}
+
+function ActiveOnVisible__checkAndActive() { 
+  $('.active-on-visible:not(.actived)').each(function(index, node) {
+      var $node = $(node);
+
+      var offsetTop = $node.attr('data-active-on-visible-offsetTop') * 1;
+      var diffY = parseInt($node.attr('data-active-on-visible-diff-y'));
+      var delay = parseInt($node.attr('data-active-on-visible-delay'));
+
+      var callbackFuncName = $node.attr('data-active-on-visible-callback-func-name');
+
+      if ( $(window).scrollTop() + $(window).height() + diffY > offsetTop ) {
+          $node.addClass('actived');
+
+          setTimeout(function() {
+              $node.addClass('active');
+              if ( window[callbackFuncName] ) {
+                  window[callbackFuncName]($node);
+              }
+          }, delay);
+      }
+  });
+}
+
+$(function() {
+  ActiveOnVisible__init();
+})
+/* 발견되면 활성화시키는 라이브러리 끝 */
+
+// 커스텀 시작
+function count($node) {
+  NumAni__start('.num');
+}
+// 커스텀 끝
