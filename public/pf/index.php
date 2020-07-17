8b0557abae562b951e27622645fbacb292c81d65
her2237 <her2237@gmail.com>
2020-07-17 18:40:41 +0900

    연결

--- public/pf/index.php
+++ public/pf/index.php
@@ -1,26 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
+    <h1> Coding </h1>
+    <nav>
+        <ul>
+            <li>
+                <a href="/pf/duotone">듀오톤</a>
+            </li>
+            <ul><li><a href="https://duotone.io/">duotone 원본</a></li></ul>
+        </ul>
+        <ul>
+            <li>
+                <a href="/pf/HYUNDAI">현대자동차</a>
+            </li>
+            <ul><li><a href="https://www.hyundai.com/kr/ko/e">현대자동차 원본</a></li></ul>
+        </ul>
+        <ul>
+            <li>
+                <a href="/pf/밀알복지재단">밀알복지재단</a>
+            </li>
+            <ul><li><a href="http://www.miral.org/main/main.asp">밀알복지재단 원본</a></li></ul>
+        </ul>
+    </nav>
+
+<h1>Design</h1>
     <ul>
         <li>
-            <a href="/pf/duotone">듀오톤</a>
-        </li>
-    </ul>
-    <ul>
-        <li>
-           <a href="/pf/HYUNDAI">현대자동차</a>
-        </li>
-    </ul>
-    <ul>
-        <li>
-           <a href="/pf/밀알복지재단">밀알복지재단</a>
+            <a href="https://her2237.github.io/img1/pf/artworks/1/1.png">팜플렛 해녀</a>
         </li>
     </ul>
 </body>
+
 </html>
