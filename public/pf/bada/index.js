8ce1a9b126cb5a38ddcf3d45af252eb3cd055093
her2237 <her2237@gmail.com>
2020-07-27 16:34:43 +0900

    바다 추가

--- public/pf/bada/index.js
+++ public/pf/bada/index.js
@@ -1,15 +1,90 @@
 /*클릭하면 언어바 변경*/
 function LangSelect__init() {
-    $('.lang-select').click(function() {
-      $(this).toggleClass('active');
-    });
-    
-    $('.lang-select > ul > li').click(function() {
-      $(this).addClass('flex-order-m1');
-      $(this).siblings('.flex-order-m1').removeClass('flex-order-m1');
-    });
-  }
-  
-  $(function() {
-    LangSelect__init();
+  $('.lang-select').click(function() {
+    $(this).toggleClass('active');
   });
+  
+  $('.lang-select > ul > li').click(function() {
+    $(this).addClass('flex-order-m1');
+    $(this).siblings('.flex-order-m1').removeClass('flex-order-m1');
+  });
+  
+}
+
+
+
+/* 발견되면 활성화시키는 라이브러리 시작 */
+function ActiveOnVisible__init() {
+    $(window).resize(ActiveOnVisible__initOffset);
+    ActiveOnVisible__initOffset();
+
+    $(window).scroll(ActiveOnVisible__checkAndActive);
+    ActiveOnVisible__checkAndActive();
+}
+
+function ActiveOnVisible__initOffset() {
+    $('.active-on-visible').each(function(index, node) {
+        var $node = $(node);
+
+        var offsetTop = $node.offset().top;
+        $node.attr('data-active-on-visible-offsetTop', offsetTop);
+
+        if ( !$node.attr('data-active-on-visible-diff-y') ) {
+            $node.attr('data-active-on-visible-diff-y', '0');
+        }
+
+        if ( !$node.attr('data-active-on-visible-delay') ) {
+            $node.attr('data-active-on-visible-delay', '0');
+        }
+    });
+
+    ActiveOnVisible__checkAndActive();
+}
+
+function ActiveOnVisible__checkAndActive() { 
+    $('.active-on-visible:not(.actived)').each(function(index, node) {
+        var $node = $(node);
+
+        var offsetTop = $node.attr('data-active-on-visible-offsetTop') * 1;
+        var diffY = parseInt($node.attr('data-active-on-visible-diff-y'));
+        var delay = parseInt($node.attr('data-active-on-visible-delay'));
+
+        var callbackFuncName = $node.attr('data-active-on-visible-callback-func-name');
+
+        if ( $(window).scrollTop() + $(window).height() + diffY > offsetTop ) {
+            $node.addClass('actived');
+
+            setTimeout(function() {
+                $node.addClass('active');
+                if ( window[callbackFuncName] ) {
+                    window[callbackFuncName]($node);
+                }
+            }, delay);
+        }
+    });
+}
+
+
+   
+/* 발견되면 활성화시키는 라이브러리 끝 */
+
+function ShipAniBox__init() {
+  var path = anime.path('.ship-ani-box path');
+
+  var motionPath = anime({
+    targets: '.ship-ani-box .follow-path',
+    translateX: path('x'),
+    translateY: path('y'),
+    rotate: path('angle'),
+    easing: 'easeInOutQuad',
+    duration: 3000,
+    loop: false 
+  });
+}
+
+$(function() {
+  LangSelect__init();
+  ActiveOnVisible__init();
+  ShipAniBox__init();
+});
+
--- public/pf/bada/index.php
+++ public/pf/bada/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,10 +20,15 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.0/anime.min.js"></script>
     <link rel="stylesheet" href="/pf/bada/index.css">
 
     <script src="/pf/bada/index.js"></script>
 </head>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.0/anime.min.js"></script>
 
 <div class="top-bar flex">
     <div class="logo-bar flex flex-ai-c">
@@ -31,10 +37,18 @@
     <nav class="menu-box-1 flex flex-1-0-0 flex-jc-end">
         <ul class="flex height-100p ">
             <li><a href="#" class="height-100p flex flex-ai-c">Bada Platform</a>
-                <ul></ul>
+                <ul>
+                    <li><a href="#">Bada-device Platform </a></li>
+                    <li><a href="#">Bada-IoT Platform</a></li>
+                    <li><a href="#">Bada-Big data Platform</a></li>
+                    <li><a href="#">Application Service</a></li>
+                </ul>
             </li>
             <li><a href="#" class="height-100p flex flex-ai-c">Developers</a>
-                <ul></ul>
+                <ul class="flex-jc-c flex-ai-c">
+                    <li><a href="#">Supports</a></li>
+                    <li><a href="#">Q&A</a></li>
+                </ul>
             </li>
             <li><a href="#" class="height-100p flex flex-ai-c">Notice</a></li>
             <li>
@@ -42,15 +56,335 @@
 
             </li>
         </ul>
-        <div class="lang-select flex-as-c">
-            <ul class="flex flex-column-nowrap">
-                <li><a class="flex flex-ai-c flex-jc-c"><span>KOR</span><span>▼</span></a></li>
+        <div class="lang-select">
+            <ul class="flex flex-column-nowrap">                
                 <li class="flex-order-m1"><a class="flex flex-ai-c flex-jc-c"><span>ENG</span><span>▼</span></a></li>
+                <li><a class="flex flex-ai-c flex-jc-c"><span>KOR</span><span>▼</span></a></li>
             </ul>
         </div>
     </nav>
 
 </div>
-<div class="main-page">
 
+<div class="main-page">
+    <div class="main-icon">
+        <div class="main-text">
+            <div>Bada Platform</div>
+            <div>Powerful IoT/Big Data Platform</div>
+            <div>for Ship and Shipbuilding Industry</div>
+        </div>
+        <div class="main-img flex">
+            <div class="img-box flex-jc-end">
+                <img src="https://her2237.github.io/img1/pf/bada/main_visual.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="bodo flex">
+        <div class="text-box flex flex-jc-start">
+            <ul>
+                <li><a href="#">[언론보도]요동치는 원유시장... 조선업계,</a></li>
+                <li><a href="#">VLCC 수주 정조준(EBN,2019.08.28)</a>
+
+                </li>
+                <li><a href="#">2019.08.29</a></li>
+            </ul>
+            <ul>
+                <li><a href="#">[언론보도]선박 수주, 석달연속 중국 제치고</a></li>
+                <li><a href="#">1위(동아닷컴,2019.08.14)</a>
+                </li>
+                <li><a href="#">2019.08.29</a></li>
+            </ul>
+            <ul>
+                <li><a href="#">[언론보도]"삼성중공업, 아프라막급 탱커</a></li>
+                <li><a href="#">수주 긍정적"(한국경제,2019,08,22)</a>
+
+                </li>
+                <li><a href="#">2019.08.29</a></li>
+            </ul>
+        </div>
+        <div class="btn-icon flex flex-ai-c ">
+            <a href="#">
+                <span class="img-box flex flex-jc-c">
+                    <img src="https://her2237.github.io/img1/pf/bada/btn_go_notice.png" alt="">
+                </span>
+            </a>
+        </div>
+    </div>
+</div>
+
+<div class="page-2 side">
+    <div class="sub-page-1 flex flex-ai-c flex-jc-c">
+        <div class="big-title">
+            <div>조선해양 IoT, 빅데이터 인프라</div>
+            <div>IoT/Big Data Infrastructure for Ship and Shipbuilding Industry</div>
+        </div>
+    </div>
+
+    <div class="sub-page-2">
+        <div class=" menu flex flex-ai-c flex-jc-c">
+            <ul class="line">
+                <li class="sub-title">조선해양 IoT 인프라</li>
+                <li class="sub-text">
+                    조선해양산업 분야에서 요구하는 응용 서비스들을 제공하기 위하여 선박, 야드 등에
+                    </br>
+                    존재하는 센서·기기 · 장치들로부터 표준화된 방식으로 실시간 데이터를 수집 · 제어 · 관리하고
+                    </br>
+                    공통의 서비스 기능을 제공하기 위한 IoT 플랫폼 및 클라우드 인프라입니다.
+                </li>
+            </ul>
+            <div class="img-box-1 active-on-visible">
+                <img src="https://her2237.github.io/img1/pf/bada/img_infra01.png" alt="">
+            </div>
+        </div>
+
+        <div class="ship-ani-box active-on-visible" data-active-on-visible-callback-func-name="ShipAniBox__init">
+            <div class="follow-path"><img src="http://www.badaplatform.org/images/main/icon_ship.png" alt=""></div>
+            <div class="outerWrapper">
+                <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+                <svg version="1.1" id="Isolation_Mode" xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="476px" height="267px"
+                    viewBox="1.75 0.375 476 267" enable-background="new 1.75 0.375 476 267" xml:space="preserve">
+                    <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="-8.4348" y1="7.8242" x2="339.801"
+                        y2="7.8242" gradientTransform="matrix(1.36 0 0 1.36 16.4391 122.2301)">
+                        <stop offset="0" style="stop-color:#6A9AF0" />
+                        <stop offset="1" style="stop-color:#EEF4FE" />
+                    </linearGradient>
+                    <path fill="#FFFFFF" stroke="url(#SVGID_1_)" stroke-width="10" stroke-linecap="round"
+                        stroke-linejoin="round" stroke-miterlimit="10"
+                        d="M11.768,8c0,0,24.444,139.932,229.214,153.464c207.453,13.712,230.786,96.28,230.786,96.28" />
+                </svg>
+            </div>
+        </div>
+
+        <div class="menu flex flex-ai-c flex-jc-c">
+            <div class="img-box-1 active-on-visible">
+                <img src="https://her2237.github.io/img1/pf/bada/img_infra02.png" alt="">
+            </div>
+            <ul class="line">
+                <li class="sub-title">조선해양 빅데이터 인프라</li>
+                <li class="sub-text">
+                    조선해양산업 생애주기에서 생성되는
+                    </br>
+                    실시간 정형/비정형 데이터의 분석/예측 기능을 지원하는 서비스 인프라입니다.
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="page-3 side">
+    <div class="in-page">
+        <div class="logo flex flex-ai-c flex-jc-c">
+            <img src="https://her2237.github.io/img1/pf/bada/img_mainTit.png" alt="">
+        </div>
+    </div>
+    <div class="grey-title flex">
+        <div>SHIPYARD</div>
+        <div>PLATFORM</div>
+    </div>
+
+    <div class="menu-bar flex flex-jc-c">
+        <div class="list-bar flex flex-jc-c flex-ai-c">
+            <div class="list list-1">
+                <div class="blue-title">Bada-Device</div>
+                <ul>
+                    <li class="a-box">
+                        <div class="icon-img flex flex-jc-c">
+                            <span class="img-box icon">
+                                <div class="txt-box">
+                                    공통기능
+                                    <br>
+                                    IoT 디바이스
+                                </div>
+                                <img src="https://her2237.github.io/img1/pf/bada/icon_device01.png" alt="">
+                            </span>
+                            <span class="img-box">
+                                <div class="txt-box">
+                                    단말탑재
+                                    <br>
+                                    IoT SW 플랫폼
+                                </div>
+                                <img src="https://her2237.github.io/img1/pf/bada/icon_device02.png" alt="">
+                            </span>
+                        </div>
+                        <dl class="flex-jc-c">
+                            <dd>조선해양 장비 연동</dd>
+                            <dd>공통기능 IoT Device</dd>
+                            <dd>공통기능 IoT Gateway</dd>
+                        </dl>
+                    </li>
+                    <li class="a-box b-box">
+                        <div class="img-icon-1 flex flex-ai-c flex-jc-c">
+                            <span class="img-box flex-ai-c flex-jc-c">
+                                <img src="https://her2237.github.io/img1/pf/bada/icon_device2_01.png" alt="">
+                                <div>용접기</div>
+                            </span>
+                            <span class="img-box">
+                                <img src="https://her2237.github.io/img1/pf/bada/icon_device2_02.png" alt="">
+                                <div>지게차</div>
+                            </span>
+                            <span class="img-box">
+                                <img src="https://her2237.github.io/img1/pf/bada/icon_device2_03.png" alt="">
+                                <div>크레인</div>
+                            </span>
+                        </div>
+                        <div class="img-icon-2 flex">
+                            <span class="img-box">
+                                <img src="https://her2237.github.io/img1/pf/bada/icon_device2_04.png" alt="">
+                                <div>물류</div>
+                            </span>
+                            <span class="img-box">
+                                <img src="https://her2237.github.io/img1/pf/bada/icon_device2_06.png" alt="">
+                                <div>작업자</div>
+                            </span>
+                            <span class="img-box">
+                                <img src="https://her2237.github.io/img1/pf/bada/icon_device2_07.png" alt="">
+                                <div>기타장비</div>
+                            </span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="list-2">
+                <div>
+                    <li class="circle-bar">
+                        <div>실시간/고신뢰 데이터수집</div>
+                    </li>
+                    <li class="circle-bar">
+                        <div>국제표준(oneM2M)기반</div>
+                    </li>
+                    <li class="circle-bar">
+                        <div>조선해양 IoT 플랫폼연동</div>
+                    </li>
+                </div>
+            </div>
+            <div class="list lsit-3">
+                <div class="blue-title">Bada-IoTs</div>
+                <ul>
+                    <li class="a-box">
+                        <div class="icon-img flex flex-jc-c">
+                            <span class="img-box icon">
+                                <div class="txt-box">
+                                    실시간 고신뢰 IoT 플랫폼
+                                </div>
+                                <img src="https://her2237.github.io/img1/pf/bada/icon_iot.png" alt="">
+                            </span>
+                        </div>
+                        <dl class="flex-jc-c">
+                            <dd>데이터 수집/저장/관리</dd>
+                            <dd>oneM2M 표준 준수</dd>
+                            <dd>원격 디바이스 제어/관리</dd>
+                            <dd>실시간 위치정보</dd>
+                        </dl>
+                    </li>
+                </ul>
+            </div>
+            <div class="list list-4">
+                <div class="blue-title">Bada-Big Data</div>
+                <ul>
+                    <li class="a-box">
+                        <div class="icon-img flex flex-jc-c">
+                            <span class="img-box icon">
+                                <div class="txt-box">
+                                    빅데이터 플랫폼
+                                </div>
+                                <img src="https://her2237.github.io/img1/pf/bada/icon_data01.png" alt="">
+                            </span>
+                        </div>
+                        <dl class="flex-jc-c">
+                            <dd>IoT 플랫폼 연동</dd>
+                            <dd>조선해양 데이터 분석</dd>
+                        </dl>
+                    </li>
+                    <li class="a-box">
+                        <div class="icon-img flex flex-jc-c">
+                            <span class="img-box icon">
+                                <div class="txt-box">
+                                    서비스 클라우드
+                                </div>
+                                <img src="https://her2237.github.io/img1/pf/bada/icon_device01.png" alt="">
+                            </span>
+                        </div>
+                        <dl class="flex-jc-c">
+                            <dd>IoT 플랫폼 연동</dd>
+                            <dd>조선해양 서비스 지원</dd>
+                        </dl>
+                    </li>
+                </ul>
+            </div>
+            <div class="list list-5">
+                <div class="blue-title">Bada-Service</div>
+                <ul>
+                    <li class="a-box flex">
+                        <div class="icon-img flex flex-ai-c">
+                            <span class="img-box icon">
+                                <img src="https://her2237.github.io/img1/pf/bada/icon_service02.png" alt="">
+                            </span>
+                            <div class="txt-box">
+                                HSE
+                                <div class="article">
+                                    HSE:Health,Safety and
+                                    the Environment
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="a-box">
+                        <div class="icon-img flex">
+                            <span class="img-box icon flex">
+                                <img src="https://her2237.github.io/img1/pf/bada/icon_service03.png" alt="">
+
+                            </span>
+                            <div class="txt-box">
+                                물류/PLM
+                                <div class="article">
+                                    PLM : Product Lifecycle
+                                    Mnagement
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="a-box">
+                        <div class="icon-img flex">
+                            <span class="img-box icon flex">
+                                <img src="https://her2237.github.io/img1/pf/bada/icon_service04.png" alt="">
+
+                            </span>
+                            <div class="txt-box flex flex-jc-c flex-ai-c">
+                                예지보전
+                            </div>
+                        </div>
+                    </li>
+                    <li class="a-box flex">
+                        <div class="icon-img flex flex-jc-c">
+                            <span class="img-box icon flex">
+                                <img src="https://her2237.github.io/img1/pf/bada/icon_service05.png" alt="">
+
+                            </span>
+                            <div class="txt-box">
+                                Etc.
+                            </div>
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="bottom-bar  flex flex-ai-c flex-jc-c">
+    <div class="in-text">
+        <ul class="menu-1 flex flex-ai-c flex-jc-c">
+            <li class="list-1">개인보호정책</li>
+            <li class="list-1">저작권정책</li>
+            <li class="list-1">이메일주소무단수집거부</li>
+        </ul>
+        <ul class="menu-2 flex flex-ai-c flex-jc-c">
+            <li class="list-box-1">주소 : 13509 경기도 성남시 분당구 새나리로 25 (야탑동) 전자부품연구원</li>
+            <li class="list-box-2">전화: 031-789-7582</li>
+            <li class="list-box-3">팩스: 031-789-7589</li>
+        </ul>
+        <div class="menu-3">Copyright 2019 © Korea Electronics Technology Institute All rights reserved.</div>
+    </div>
 </div>
